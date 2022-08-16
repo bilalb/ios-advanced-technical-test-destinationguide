58a46b1bd3e6a690783d8b03c5ae97ae8cf37e0d
Bilal Benlarbi <bilal@onlyonecard.eu>
2022-08-16 18:32:59 +0200

    unused `UICollectionViewDelegateFlowLayout` removed

--- DestinationGuide/Scenes/List/DestinationsViewController.swift
+++ DestinationGuide/Scenes/List/DestinationsViewController.swift
@@ -8,7 +8,7 @@
 import Combine
 import UIKit
 
-final class DestinationsViewController: UIViewController, UICollectionViewDelegateFlowLayout {
+final class DestinationsViewController: UIViewController {
     private let viewModel: ViewModel
     private var cancellables: Set<AnyCancellable> = []
 
@@ -137,18 +137,6 @@ private extension DestinationsViewController {
                 activityIndicator.stopAnimating()
             }
             .store(in: &cancellables)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        
-        // Get the view for the first header
-        let indexPath = IndexPath(row: 0, section: section)
-        let headerView = self.collectionView(collectionView, viewForSupplementaryElementOfKind: UICollectionView.elementKindSectionHeader, at: indexPath)
-        
-        // Use this view to calculate the optimal size based on the collection view's width
-        return headerView.systemLayoutSizeFitting(CGSize(width: collectionView.frame.width, height: UIView.layoutFittingExpandedSize.height),
-                                                  withHorizontalFittingPriority: .required, // Width is fixed
-                                                  verticalFittingPriority: .fittingSizeLevel) // Height can be as large as needed
     }
 }
 
